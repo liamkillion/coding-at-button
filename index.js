ed74667eaf8efa5cc9bde73ef13f47178edf6466
Liam Killion <liam.killion@gmail.com>
2018-03-15 10:04:01 -0400

    possible solution

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
- // expressionEvalutator = function(operator, expressiona, expressionb) {
+ // expressionEvaluator = function(operator, expressiona, expressionb) {
  //   if (operator.toString() === "+") {
  //     return expressiona + expressionb
  //   } else {
@@ -10,27 +10,60 @@
  //
  //   ["+", "1", "+", "2", "3"]
 
- let expressionEvalutator = function(expression) {
+ // let expressionEvaluator = function(expression) {
+ //   let array = expression.split(" ")
+ //   let solution = 0
+ //   if (array.length > 3) {
+ //     if (array.first === "+") {
+ //       if (array[2] === "+") {
+ //         solution = array[1] + array[3] + array.last
+ //       } else {
+ //         solution = array[1] + array[3] - array.last
+ //       }
+ //     } else {
+ //       if (array[2] === "+") {
+ //         solution = array[1] - array[3] + array.last
+ //       } else {
+ //         solution = array[1] - array[3] - array.last
+ //       }
+ //     }
+ //   } else {
+ //     if (array.first === "+") {
+ //       solution = array[1] + array.last
+ //     } else {
+ //       solution = array[1] - array.last
+ //     }
+ //   }
+ //   return solution
+ // }
+ //  <operation> <expression> <expression>
+ // "+ 1 + 2 3" === 6
+ //
+ //   ["+", "1", "+", "2", "3"]
+
+ //   ["+", "1", "+", "2", "3"]
+ //   ["+", "+", "1", "2", "3"]
+
+ let expressionEvaluator = function(expression) {
    let array = expression.split(" ")
-   if (array.length > 3) {
-     if (array.first === "+") {
-       if (array[2] === "+") {
-         return array[1] + array[3] + array.last
-       } else {
-         return array[1] + array[3] - array.last
-       }
+   let operations = []
+   let numerals = []
+   array.forEach(element => {
+     if (parseInt(element)) {
+       numerals.push(element)
      } else {
-       if (array[2] === "+") {
-         return array[1] - array[3] + array.last
-       } else {
-         return array[1] - array[3] - array.last
-       }
+       operations.push(element)
      }
-   } else {
-     if (array.first === "+") {
-       return array[1] + array.last
-     } else {
-       return array[1] - array.last
-     }
+   })
+   let newArray = []
+   for (i = 0; i < numerals.length; i++) {
+     newArray.push(numerals[i])
+     newArray.push(operations[i])
    }
+   let solution = newArray.join('')
+   return eval(solution)
+ }
+
+ export {
+   expressionEvaluator
  }
